9cbe7b30f36af632fbc9db84d9f3ea05c4d47837
moshiur <rmoshiur705@gmail.com>
2017-11-25 20:37:01 +0600

    add spring core,beans & context jar

---
+++ src/main/java/edu/learning/springcore/beanconfig/App.java
@@ -0,0 +1,13 @@
+package edu.learning.springcore.beanconfig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
